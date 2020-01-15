b846685c62d262bdd218006145720b334c040d03
Alberto Toledo <albtolmay@alum.us.es>
2020-01-15 23:27:25 +0100

    Last fixes

--- src/main/java/acme/features/administrator/dashboard/AdministratorDashboardRepository.java
+++ src/main/java/acme/features/administrator/dashboard/AdministratorDashboardRepository.java
@@ -20,15 +20,6 @@ public interface AdministratorDashboardRepository extends AbstractRepository {
 	@Query("select count(ir) from InvestorRecord ir")
 	Integer numberOfInvestorRecords();
 
-	@Query("select min(r.reward.amount), max(r.reward.amount), avg(r.reward.amount), stddev(r.reward.amount) from Request r where r.deadline > CURRENT_TIMESTAMP")
-	Collection<Object[]> activeRequestsRewardStats();
-
-	@Query("select min(o.minReward.amount), max(o.minReward.amount), avg(o.minReward.amount), stddev(o.minReward.amount) from Offer o  where o.deadline > CURRENT_TIMESTAMP")
-	Collection<Object[]> activeOffersMinRewardStats();
-
-	@Query("select min(o.maxReward.amount), max(o.maxReward.amount), avg(o.maxReward.amount), stddev(o.maxReward.amount) from Offer o where o.deadline > CURRENT_TIMESTAMP")
-	Collection<Object[]> activeOffersMaxRewardStats();
-
 	@Query("select cr.sector, count(cr) from CompanyRecord cr group by cr.sector")
 	Collection<Object[]> companiesBySector();
 
--- src/main/java/acme/features/administrator/dashboard/AdministratorDashboardShowService.java
+++ src/main/java/acme/features/administrator/dashboard/AdministratorDashboardShowService.java
@@ -37,24 +37,6 @@ public class AdministratorDashboardShowService implements AbstractShowService<Ad
 		Integer numberOfCompanyRecords = this.repository.numberOfCompanyRecords();
 		Integer numberOfInvestorRecords = this.repository.numberOfInvestorRecords();
 
-		List<Object[]> rewardActiveRequestsStats = new ArrayList<Object[]>(this.repository.activeRequestsRewardStats());
-		Double minimumRewardActiveRequests = (Double) rewardActiveRequestsStats.get(0)[0];
-		Double maximumRewardActiveRequests = (Double) rewardActiveRequestsStats.get(0)[1];
-		Double averageRewardActiveRequests = (Double) rewardActiveRequestsStats.get(0)[2];
-		Double stddevRewardActiveRequests = (Double) rewardActiveRequestsStats.get(0)[3];
-
-		List<Object[]> minRewardActiveOffersStats = new ArrayList<Object[]>(this.repository.activeOffersMinRewardStats());
-		Double minimumMinRewardActiveOffers = (Double) minRewardActiveOffersStats.get(0)[0];
-		Double maximumMinRewardActiveOffers = (Double) minRewardActiveOffersStats.get(0)[1];
-		Double averageMinRewardActiveOffers = (Double) minRewardActiveOffersStats.get(0)[2];
-		Double stddevMinRewardActiveOffers = (Double) minRewardActiveOffersStats.get(0)[3];
-
-		List<Object[]> maxRewardActiveOffersStats = new ArrayList<Object[]>(this.repository.activeOffersMaxRewardStats());
-		Double minimumMaxRewardActiveOffers = (Double) maxRewardActiveOffersStats.get(0)[0];
-		Double maximumMaxRewardActiveOffers = (Double) maxRewardActiveOffersStats.get(0)[1];
-		Double averageMaxRewardActiveOffers = (Double) maxRewardActiveOffersStats.get(0)[2];
-		Double stddevMaxRewardActiveOffers = (Double) maxRewardActiveOffersStats.get(0)[3];
-
 		List<Object[]> companiesAndSectors = new ArrayList<Object[]>(this.repository.companiesBySector());
 		List<String> cSectors = new ArrayList<String>();
 		List<Long> companiesBySector = new ArrayList<Long>();
@@ -75,30 +57,13 @@ public class AdministratorDashboardShowService implements AbstractShowService<Ad
 		entity.setNumberOfCompanyRecords(numberOfCompanyRecords);
 		entity.setNumberOfInvestorRecords(numberOfInvestorRecords);
 
-		entity.setMinimumRewardActiveRequests(minimumRewardActiveRequests);
-		entity.setMaximumRewardActiveRequests(maximumRewardActiveRequests);
-		entity.setAverageRewardActiveRequests(averageRewardActiveRequests);
-		entity.setStddevRewardActiveRequests(stddevRewardActiveRequests);
-
-		entity.setMinimumMinRewardActiveOffers(minimumMinRewardActiveOffers);
-		entity.setMaximumMinRewardActiveOffers(maximumMinRewardActiveOffers);
-		entity.setAverageMinRewardActiveOffers(averageMinRewardActiveOffers);
-		entity.setStddevMinRewardActiveOffers(stddevMinRewardActiveOffers);
-
-		entity.setMinimumMaxRewardActiveOffers(minimumMaxRewardActiveOffers);
-		entity.setMaximumMaxRewardActiveOffers(maximumMaxRewardActiveOffers);
-		entity.setAverageMaxRewardActiveOffers(averageMaxRewardActiveOffers);
-		entity.setStddevMaxRewardActiveOffers(stddevMaxRewardActiveOffers);
-
 		entity.setCSectors(cSectors);
 		entity.setCompaniesBySector(companiesBySector);
 
 		entity.setISectors(iSectors);
 		entity.setInvestorsBySector(investorsBySector);
 
-		request.unbind(entity, model, "numberOfAnnouncements", "numberOfCompanyRecords", "numberOfInvestorRecords", "minimumRewardActiveRequests", "maximumRewardActiveRequests", "averageRewardActiveRequests", "stddevRewardActiveRequests",
-			"minimumMinRewardActiveOffers", "maximumMinRewardActiveOffers", "averageMinRewardActiveOffers", "stddevMinRewardActiveOffers", "minimumMaxRewardActiveOffers", "maximumMaxRewardActiveOffers", "averageMaxRewardActiveOffers",
-			"stddevMaxRewardActiveOffers", "cSectors", "companiesBySector", "iSectors", "investorsBySector");
+		request.unbind(entity, model, "numberOfAnnouncements", "numberOfCompanyRecords", "numberOfInvestorRecords", "cSectors", "companiesBySector", "iSectors", "investorsBySector");
 
 	}
 
